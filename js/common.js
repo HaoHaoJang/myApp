d44d289e54b4bee4a87cc928f82edef6ae9bda18
鼠浩陈 <n_shchen@3songshu.com>
2020-07-18 21:39:40 +0800

    ad

--- js/common.js
+++ js/common.js
@@ -84,6 +84,7 @@ function ajaxMothed(url, params, type) {
     timeout: 100000,
     contentType: "application/json",
     success: function (data) {
+      console.log(data, 333333)
       var data = JSON.parse(data);
       if (data.success) {
         defer.resolve(data);
@@ -158,4 +159,23 @@ function pageScroll(callback) {
       _scrollFresh = true;
       callback();
     });
+}
+function dateFormat(fmt, date) {
+  let ret;
+  const opt = {
+    "y+": date.getFullYear().toString(),        // 年
+    "m+": (date.getMonth() + 1).toString(),     // 月
+    "d+": date.getDate().toString(),            // 日
+    "H+": date.getHours().toString(),           // 时
+    "M+": date.getMinutes().toString(),         // 分
+    "S+": date.getSeconds().toString()          // 秒
+    // 有其他格式化字符需求可以继续添加，必须转化成字符串
+  };
+  for (let k in opt) {
+    ret = new RegExp("(" + k + ")").exec(fmt);
+    if (ret) {
+      fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+    };
+  };
+  return fmt;
 }
